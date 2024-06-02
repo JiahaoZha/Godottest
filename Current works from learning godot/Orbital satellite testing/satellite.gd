f8dc879f022caf05a83700bda116c44a0ee811a1
JiahaoZha <107592370+JiahaoZha@users.noreply.github.com>
2024-06-01 20:37:24 -0500

    add trial

--- Current works from learning godot/Orbital satellite testing/satellite.gd
+++ Current works from learning godot/Orbital satellite testing/satellite.gd
@@ -10,15 +10,14 @@ var y = transform.origin.y
 func _ready():
 	transform.origin = transform.origin
 	current_angle = 0.0
-	
-	print("Orbiting planet is ready")
+
 
 func _process(delta):
-	current_angle += orbit_speed*delta
+	current_angle = current_angle + orbit_speed*delta
 	if current_angle >= 360.0:
 		current_angle -= 360.0
-	var x = planet.transform.origin.x+orbit_radius * cos(deg_to_rad((current_angle)))
-	var z = planet.transform.origin.z+orbit_radius * sin(deg_to_rad((current_angle)))
+	var x = planet.transform.origin.x + orbit_radius * cos(deg_to_rad((current_angle)))
+	var z = planet.transform.origin.z + orbit_radius * sin(deg_to_rad((current_angle)))
 	transform.origin = Vector3(x, y, z)
 
 
